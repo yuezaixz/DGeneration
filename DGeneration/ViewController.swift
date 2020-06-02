8a361486c868b93a35565fedcce59a9f7ae71726
yuezaixz <303178394@qq.com>
2020-06-02 22:47:20 +0800

    useless code

--- DGeneration/ViewController.swift
+++ DGeneration/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if #available(iOS 11.0, *) {
+            scrollView.contentInsetAdjustmentBehavior = .never
+        } else {
+            self.automaticallyAdjustsScrollViewInsets = false
+        }
+        
+        let testView = UIView()
+        testView.backgroundColor = UIColor.red
+        scrollView.addSubview(testView)
+        let label = UILabel()
+        label.text = "测试"
+        testView.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        testView.snp.makeConstraints { make in
+            make.width.edges.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(2)
+        }
     }
 
 
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(1)
+    }
+}
